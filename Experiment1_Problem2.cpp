830799de4823bd4ba56ebd88b84c60c2178efa69
John Lerry S. Canilla <47727545+jlcanilla@users.noreply.github.com>
2019-03-26 21:36:16 +0800

    Add files via upload

---
+++ Experiment1_Problem2.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+
+using namespace std;
+
+int main()
+{
+    double mass,density,volume;
+
+    cout << setprecision(2) << showpoint << fixed;
+    cout << "Enter mass: " ;
+    cin >> mass;
+    cout << "Enter density: ";
+    cin >> density;
+
+    vokume = mass / density ;
+
+    cout << "Volume is " << volume <<endl;
+	getch ();
+    return 0;
+}		
